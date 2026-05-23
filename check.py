0b1b395a6d70b550a062d1109214a7a196619a98
maxidztt <tolediazmaxi@gmail.com>
2026-05-23 20:31:48 -0300

    Include adult soccer boots in alerts

--- check.py
+++ check.py
@@ -13,7 +13,7 @@ BASE_API_URL = os.getenv(
 PRECIO_MAXIMO = int(os.getenv("PRECIO_MAXIMO", "39000"))
 PAGINAS_A_REVISAR = int(os.getenv("PAGINAS_A_REVISAR", "5"))
 PRODUCTOS_POR_PAGINA = int(os.getenv("PRODUCTOS_POR_PAGINA", "24"))
-PALABRAS_PERMITIDAS = ("zapatilla", "zapatillas", "crocs")
+PALABRAS_PERMITIDAS = ("zapatilla", "zapatillas", "crocs", "botin", "botines")
 PALABRAS_EXCLUIDAS = ("niño", "niños", "niña", "niñas", "bebe", "bebes", "bebé", "bebés")
 
 
--- test_check.py
+++ test_check.py
@@ -47,10 +47,12 @@ class SportingAlertTests(unittest.TestCase):
     def test_permite_zapatillas_y_crocs(self):
         self.assertTrue(producto_permitido("Zapatillas adidas Grand Court Lo De Mujer"))
         self.assertTrue(producto_permitido("Crocs Classic Unisex"))
+        self.assertTrue(producto_permitido("Botines Joma Aguila FG De Hombre"))
 
     def test_rechaza_ojotas_y_productos_de_ninos(self):
         self.assertFalse(producto_permitido("Ojotas Rider R1 De Mujer"))
         self.assertFalse(producto_permitido("Zapatillas Diversao Ale De Niños"))
+        self.assertFalse(producto_permitido("Botines Umbro Mutant De Niños"))
         self.assertFalse(producto_permitido("Zapatillas Puma Rickie De Bebés"))
 
 
