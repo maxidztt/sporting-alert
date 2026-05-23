290e0ad31a94f9a948e80f0087b07eacc565bdb6
maxidztt <tolediazmaxi@gmail.com>
2026-05-23 14:46:37 -0300

    Update check.py

--- check.py
+++ check.py
@@ -1,14 +1,22 @@
+
 import requests
+import sys
 
 TOKEN = "8843148366:AAGcapDQk_NcjVmVkR-pahZeObjSrq_SNcA"
 CHAT_ID = "7727821551"
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-response = requests.post(url, data={
-    "chat_id": CHAT_ID,
-    "text": "🔥 TEST DEFINITIVO"
-})
+response = requests.post(
+    url,
+    json={
+        "chat_id": CHAT_ID,
+        "text": "🔥 FUNCIONA EL BOT"
+    },
+    timeout=20
+)
 
-print(response.status_code)
-print(response.text)
+print("STATUS:", response.status_code, flush=True)
+print("RESPUESTA:", response.text, flush=True)
+
+sys.stdout.flush()
