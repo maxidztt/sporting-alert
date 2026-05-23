7158cbdc6c5df51c3c8babe2afbfa02e19094a00
maxidztt <tolediazmaxi@gmail.com>
2026-05-23 14:35:30 -0300

    Update check.py

--- check.py
+++ check.py
@@ -1,38 +1,15 @@
 import requests
-from bs4 import BeautifulSoup
 
 TOKEN = "8843148366:AAGcapDQk_NcjVmVkR-pahZeObjSrq_SNcA"
 CHAT_ID = "7727821551"
 
-URL = "https://www.sporting.com.ar/sporting/calzado"
+mensaje = "🔥 TEST OK - el bot funciona"
 
-PRECIO_LIMITE = 1000000
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-headers = {
-    "User-Agent": "Mozilla/5.0"
-}
+r = requests.get(url, params={
+    "chat_id": CHAT_ID,
+    "text": mensaje
+})
 
-r = requests.get(URL, headers=headers)
-soup = BeautifulSoup(r.text, "html.parser")
-
-productos = soup.find_all("div", class_="vtex-product-summary-2-x-container")
-
-for p in productos:
-    texto = p.get_text(" ", strip=True)
-
-    precios = [int(s.replace(".", "")) for s in texto.split() if s.replace(".", "").isdigit()]
-
-    if precios:
-        precio = min(precios)
-
-        if precio <= PRECIO_LIMITE:
-
-            mensaje = f"🔥 Oferta encontrada\n\n{texto}\n\nPrecio: ${precio}"
-
-            requests.get(
-                f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-                params={
-                    "chat_id": CHAT_ID,
-                    "text": mensaje
-                }
-            )
+print(r.text)
