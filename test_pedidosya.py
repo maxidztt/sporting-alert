dee840d404ec61060bc4bce189c7b4350ebfbdb4
maxidztt <tolediazmaxi@gmail.com>
2026-06-08 22:50:41 -0300

    Refactor code for better readability and structure

--- test_pedidosya.py
+++ test_pedidosya.py
@@ -1,16 +1,25 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)
+    browser = p.chromium.launch(
+        headless=True
+    )
 
     page = browser.new_page()
 
     page.goto(
         "https://www.pedidosya.com.ar/groceries/web/v1/vendors/183015/categories",
-        wait_until="networkidle"
+        wait_until="networkidle",
+        timeout=60000
     )
 
-    print("TITULO:", page.title())
-    print(page.content()[:2000])
+    print("URL FINAL:")
+    print(page.url)
+
+    print("\nTITULO:")
+    print(page.title())
+
+    print("\nCONTENIDO:")
+    print(page.content()[:5000])
 
     browser.close()
